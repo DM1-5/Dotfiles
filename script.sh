8dbfd01f3003c530c81d4778826a56c951a30bba
Santiago S <santiago.sepulveda1@utp.edu.co>
2021-11-19 14:57:27 -0500

    Plug install

--- script.sh
+++ script.sh
@@ -1,4 +1,8 @@
-mkdir -p ~/.config/alacritty
-cp alacritty.yml ~/.config/alacritty
+mkdir -p ~/.config/alacritty && cp alacritty.yml ~/.config/alacritty
 cp .gitconfig .vimrc ~/
+sudo apt install curl vim -y
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 
